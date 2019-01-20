b2c4ebe0f0d851f86dccd1cb77a5b36d3fa5c30e
EdoardoBaral <45716301+EdoardoBaral@users.noreply.github.com>
2019-01-20 13:40:46 +0100

    Creazione classe EntityMagazzino

---
+++ src/main/java/om/EntityMagazzino.java
@@ -0,0 +1,79 @@
+package om;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+/**
+ * EntityMagazzino. Classe che rappresenta un record nel registro di magazzino. Le istanze di questa classe descrivono un determinato dipo di prodotto presente in magazzino e la relativa giacenza
+ * (ovvero la quantità residua)
+ *
+ * @author Edoardo Baral
+ */
+
+public class EntityMagazzino
+{
+    private String nome;
+    private int giacenza;
+
+    /**
+     * Costruttore di default della classe EntityMagazzino
+     */
+    public EntityMagazzino()
+    {
+        giacenza = 0;
+    }
+
+    /**
+     * Metodo che restituisce il nome del prodotto
+     * @return il nome del prodotto
+     */
+    public String getNome()
+    {
+        return nome;
+    }
+
+    /**
+     * Metodo che permette di indicare un nuovo nome per il prodotto
+     * @param nome: nuovo nome da associare al prodotto
+     */
+    public void setNome(String nome)
+    {
+        this.nome = nome;
+    }
+
+    /**
+     * Metodo che restituisce la quantità residua di prodotto presente in magazzino (giacenza)
+     * @return la giacenza del prodotto in magazzino
+     */
+    public int getGiacenza()
+    {
+        return giacenza;
+    }
+
+    /**
+     * Metodo che permette di impostare un nuovo valore per la giacenza del prodotto
+     * @param giacenza: nuova giacenza da associare al prodotto
+     */
+    public void setGiacenza(int giacenza)
+    {
+        this.giacenza = giacenza;
+    }
+
+    /**
+     * Metodo che restituisce una rappresentazione JSON dell'oggetto
+     * @return una stringa rappresentante l'oggetto in formato JSON, null in caso d'errore
+     */
+    @Override
+    public String toString()
+    {
+        try
+        {
+            ObjectMapper mapper = new ObjectMapper();
+            return mapper.writeValueAsString(this);
+        }
+        catch(JsonProcessingException e)
+        {
+            return null;
+        }
+    }
+}
