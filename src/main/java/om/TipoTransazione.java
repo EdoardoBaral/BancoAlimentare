36c0ec90cb0f2a4e558da4a5b0f12d0488291924
EdoardoBaral <45716301+EdoardoBaral@users.noreply.github.com>
2019-01-27 15:47:40 +0100

    Creazione dell'enum TipoTransazione

---
+++ src/main/java/om/TipoTransazione.java
@@ -0,0 +1,13 @@
+package om;
+
+/**
+ * TipoTransazione. Enum che permette di distinguere i due tipi di transazioni rappresentabili dalla classe EntityRegistro:
+ * in ingresso (prodotto che entra in magazzino) o in uscita (prodotto che viene ceduto).
+ *
+ * @author Edoardo Baral
+ */
+public enum TipoTransazione
+{
+    INGRESSO,
+    USCITA
+}
