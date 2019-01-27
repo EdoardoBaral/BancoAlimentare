f7bbece4bb41222257bb31554dd4394e5996aab9
EdoardoBaral <45716301+EdoardoBaral@users.noreply.github.com>
2019-01-27 13:41:32 +0100

    Aggornata la classe di test EntityMagazzinoTest con aggiunta del test per il metodo compareTo() della classe EntityMagazzino

--- src/test/java/om/EntityMagazzinoTest.java
+++ src/test/java/om/EntityMagazzinoTest.java
@@ -22,4 +22,31 @@ public class EntityMagazzinoTest
         assertEquals("Spaghetti 1 kg", em.getNome());
         assertEquals(10, em.getGiacenza());
     }
+
+    /**
+     * Metodo che permette di testare il corretto funzionamento del metodo compareTo() implementato nella classe EntityMagazzino
+     */
+    @Test
+    public void testCompareTo()
+    {
+        EntityMagazzino em1 = new EntityMagazzino();
+        em1.setNome("Spaghetti 1 kg");
+        em1.setGiacenza(10);
+
+        EntityMagazzino em2 = new EntityMagazzino();
+        em2.setNome("Tonno in scatola 200 g");
+        em2.setGiacenza(5);
+
+        EntityMagazzino em3 = new EntityMagazzino();
+        em3.setNome("Olio d'oliva 1 L");
+        em3.setGiacenza(7);
+
+        EntityMagazzino em4 = new EntityMagazzino();
+        em4.setNome("Spaghetti 1 kg");
+        em4.setGiacenza(10);
+
+        assertTrue(em1.compareTo(em2) < 0);
+        assertTrue(em1.compareTo(em3) > 0);
+        assertTrue(em1.compareTo(em4) == 0);
+    }
 }
