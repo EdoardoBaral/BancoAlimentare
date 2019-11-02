ea9a7c728157efaa5b7bda843bd01a38e02309de
Edoardo Baral <edoardo@baral.it>
2019-11-02 19:05:53 +0100

    Rimozione metodi exists() da interfaccia EntityRegistroController

--- src/main/java/interfaces/EntityRegistroController.java
+++ src/main/java/interfaces/EntityRegistroController.java
@@ -35,20 +35,6 @@ public interface EntityRegistroController
     EntityRegistro modificaTransazione(EntityRegistro transazione);
 
     /**
-     * Metodo che verifica l'esistenza nel registro di una transazione uguale a quella passata come argomento, sfruttando l'algoritmo di ricerca binaria
-     * @param transazione: transazione da cercare nel registro
-     * @return l'indice della transazione all'interno della lista, negativo nel caso non esista
-     */
-    int exists(EntityRegistro transazione);
-
-    /**
-     * Metodo che verifica l'esistenza nel registro di una transazione il cui id è uguale a quello passato come argomento, sfruttando l'algoritmo della ricerca binaria
-     * @param id: id della transazione da cercare nel magazzino
-     * @return l'indice della transazione all'interno della lista, negativo nel caso non esista
-     */
-    int exists(Long id);
-
-    /**
      * Metodo che restituisce la lista delle transazioni presenti sul registro
      * @return la lista delle transazioni presenti sul registro
      */
